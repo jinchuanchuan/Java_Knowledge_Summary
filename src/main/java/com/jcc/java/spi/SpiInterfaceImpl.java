9d20ddca9e4f293e5332cc9848f056d9d7a5fb4e
jinchuanchuan <jinchuanchuan1@le.com>
2019-12-20 16:58:33 +0800

    add spi interface impl

---
+++ src/main/java/com/jcc/java/spi/SpiInterfaceImpl.java
@@ -0,0 +1,10 @@
+package com.jcc.java.spi;
+
+/**
+ * @program: javaknowledge->SpiInterfaceImpl
+ * @description: SPI实现类
+ * @author: jinchuanchuan1@le.com
+ * @create: 2019-12-20 16:58
+ **/
+public class SpiInterfaceImpl {
+}
