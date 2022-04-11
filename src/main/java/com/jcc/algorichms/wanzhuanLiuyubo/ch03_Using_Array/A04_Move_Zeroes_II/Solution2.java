35d4b8aebe73578b391314fa598e92683b6befa3
jinchuanchuan <sdzzjcc@163.com>
2022-04-11 20:36:18 +0800

    玩转算法面试第二章

--- src/main/java/com/jcc/algorichms/wanzhuanLiuyubo/ch03_Using_Array/A04_Move_Zeroes_II/Solution2.java
+++ src/main/java/com/jcc/algorichms/wanzhuanLiuyubo/ch03_Using_Array/A04_Move_Zeroes_II/Solution2.java
@@ -10,13 +10,17 @@ import java.util.List;
  * @create: 2021-01-13 18:46
  **/
 public class Solution2 {
+    // 时间复杂度：O（n）
+    // 空间复杂度：O（1)
     public void moveZeroes(int[] nums) {
-        int k = 0;
+        int k = 0; // nums中，[0...K)的元素均为非0元素
+        // 遍历到第i个元素后，保证[0...i]中所有非0元素，都按照顺序排列在[0...k)中
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
                 nums[k++] = nums[i];
             }
         }
+        // 将nums剩余的位置放置为0
         if(k < nums.length) {
             for (int i = k; i < nums.length; i++) {
                 nums[i] = 0;
--- src/main/java/com/jcc/algorichms/wanzhuanLiuyubo/ch03_Using_Array/A04_Move_Zeroes_II/Solution4.java
+++ src/main/java/com/jcc/algorichms/wanzhuanLiuyubo/ch03_Using_Array/A04_Move_Zeroes_II/Solution4.java
@@ -8,12 +8,20 @@ package com.jcc.algorichms.wanzhuanLiuyubo.ch03_Using_Array.A04_Move_Zeroes_II;
  **/
 public class Solution4 {
     public void moveZeroes(int[] nums) {
-        int k = 0;
+        int k = 0; // nums中, [0...k)的元素均为非0元素
+
+        // 遍历到第i个元素后,保证[0...i]中所有非0元素
+        // 都按照顺序排列在[0...k)中
+        // 同时, [k...i] 为 0
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                int temp =  nums[i];
-                nums[i] = nums[k];
-                nums[k++] = temp;
+                if (k != i) { // 特殊情况处理，如果整个数组都是非零0元素，则禁止自己和自己swap
+                    int temp =  nums[i];
+                    nums[i] = nums[k];
+                    nums[k++] = temp;
+                } else { // i == k
+                    k++;
+                }
             }
         }
     }
